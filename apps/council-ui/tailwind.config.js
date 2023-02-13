d52bb79d11da4a33d59bf3df4578ecb241c94b64
Danny Delott <dannydelott@gmail.com>
2023-02-13 15:49:55 -0800

    ElementDAO daisy theme

--- apps/council-ui/tailwind.config.js
+++ apps/council-ui/tailwind.config.js
@@ -7,5 +7,22 @@ module.exports = {
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
     prefix: "daisy-",
+    themes: [
+      {
+        elementDaoTheme: {
+          primary: "#005EBE", // principal royal blue
+          secondary: "#6ACDE2", // principal blue
+          accent: "#19C2A3", // topaz
+          neutral: "#1B1D21", // black
+          "base-100": "#F8F8F8",
+          "base-200": "#FFFFFF",
+          "base-300": "#FFFFFF",
+          info: "#B2CAE3", // blueGrey
+          success: "#007F00", //statusGreen
+          warning: "#FFC300", // goldYellow
+          error: "#F73030", // deepRed
+        },
+      },
+    ],
   },
 };
